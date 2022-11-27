fca10d6a147c11dff5cef0577b9e99766a40bb9c
unional <homawong@gmail.com>
2022-11-26 19:31:21 -0800

    refactor: convert to mono repo

---
+++ typedoc.js
@@ -0,0 +1,12 @@
+/**
+ * @type {import('typedoc').TypeDocOptions}
+ */
+module.exports = {
+  customTitle: "Run codes and ignore any error",
+  customTitleLink: "https://github.com/unional/never-fail",
+  entryPointStrategy: "packages",
+  entryPoints: [
+    "packages/never-fail"
+  ],
+  out: "docs",
+}
