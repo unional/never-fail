b435ff7b747b7c369cae55dd099b618086eeff36
unional <homawong@gmail.com>
2022-12-10 20:33:11 -0800

    fix: update exports field order
    add default order

--- packages/never-fail/jest.config.mjs
+++ packages/never-fail/jest.config.mjs
@@ -1,29 +1,4 @@
 export default {
-  displayName: 'never-fail',
-  extensionsToTreatAsEsm: ['.ts'],
-  moduleNameMapper: {
-    '^(\\.{1,2}/.*)\\.js$': '$1',
-  },
-  collectCoverageFrom: [
-    '<rootDir>/ts/**/*.[jt]s'
-  ],
-  roots: ['<rootDir>/ts'],
-  testMatch: ['**/?(*.)+(spec|test|integrate|accept|system|unit).[jt]s?(x)'],
-  transform: {
-    '^.+\\.m?[t]sx?$': ['ts-jest', {
-      isolatedModules: true,
-      useESM: true
-    }],
-  },
-  watchPlugins: [
-    'jest-watch-suspend',
-    'jest-watch-typeahead/filename',
-    'jest-watch-typeahead/testname',
-    [
-      'jest-watch-toggle-config', { 'setting': 'verbose' },
-    ],
-    [
-      'jest-watch-toggle-config', { 'setting': 'collectCoverage' },
-    ],
-  ],
+  preset: '@repobuddy/jest/presets/ts-esm-watch',
+  displayName: 'never-fail'
 }
